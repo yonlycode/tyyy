ddaa9b11b4683b60bb4633177ba4117c441bf586
yonlycode <yonlycode@users.noreply.github.com>
2026-08-18 07:21:40 +0000

    deploy: 71b147c663ba04613cf93e44d006576d1342c1e6

--- _next/static/Vv56B8ku55_BvqWryx07e/_buildManifest.js
+++ _next/static/Vv56B8ku55_BvqWryx07e/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tyyy//_next/:path+",
+        "destination": "/tyyy/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
