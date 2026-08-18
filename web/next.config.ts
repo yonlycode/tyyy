71b147c663ba04613cf93e44d006576d1342c1e6
yonly.yone <yoann.fort@outlook.com>
2026-08-18 03:20:50 -0400

    fix deployment 6

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'tyyy'; // Remplacez par le nom exact du dépôt
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+    assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true, // Requis pour l'export statique sans serveur Node
   },
